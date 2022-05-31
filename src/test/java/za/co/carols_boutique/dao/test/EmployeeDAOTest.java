c34e7145acc2a8774818ec78fca036bf27ccd134
Mustafaa Osman <Mustafaa Osman@192.168.20.223>
2022-05-31 14:47:36 +0200

    Adding junit tests.

--- src/test/java/za/co/carols_boutique/dao/test/EmployeeDAOTest.java
+++ src/test/java/za/co/carols_boutique/dao/test/EmployeeDAOTest.java
@@ -2,55 +2,54 @@ package za.co.carols_boutique.dao.test;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import za.co.carols_boutique.EmployeeBE.IDAOEmployee.DAOEmp;
 import za.co.carols_boutique.EmployeeBE.IDAOEmployee.DaoEmpImp;
 import za.co.carols_boutique.models.Employee;
 
 
 public class EmployeeDAOTest {
-	//Create default constructor employee and test the setter methods.
 	
-	DAOEmp emp;
-	
+	DaoEmpImp emp;
+
 	public EmployeeDAOTest() {
 		this.emp = new DaoEmpImp();
 	}
-	
+
 	@Test
 	void testAddEmployee() {
-		assertTrue(emp.addEmployee(new Employee("empoyeeTest1", "TestName", "TestSurname", "TestPass", false)));
+		assertTrue(emp.addEmployee(new Employee("empoyeeTestID1", "EmployeeTestName", "EmployeeTestSurname", "EmployeeTestPass", "TestStoreID", false)));
 	}
-	
+
 	Employee employee = null;
-	
+
 	@Test
 	void testGetEmployee() {
-		employee = emp.getEmployee("empoyeeTest1", "TestPass");
+		employee = emp.getEmployee("empoyeeTestID1", "TestPass", "TestStoreID");
 		assertNotNull(employee);
 	}
-	
+
 	@Test
 	void testUpdateEmployee() {
+		employee.setName("TestName2");
 		assertTrue(emp.updateEmployee(employee));
 	}
-	
+
 	@Test
 	void checkNotMannager() {
 		assertFalse(employee.getIsManager());
 	}
-	
+
 	@Test
-	void testPromoteToManager(){
-		assertTrue(emp.promoteToManager("empoyeeTest1"));
+	void testPromoteToManager() {
+		assertTrue(emp.promoteToManager("empoyeeTestID1"));
 	}
-	
+
 	@Test
 	void checkIsMannager() {
 		assertTrue(employee.getIsManager());
 	}
-	
-	@Test 
+
+	@Test
 	void testDeleteEmployee() {
-		assertTrue(emp.deleteEmployee("empoyeeTest1"));
+		assertTrue(emp.deleteEmployee("empoyeeTestID1"));
 	}
 }
