3ea66bee6cf9a0dec40c308a136c673b14d4bf26
notJeanPaul <jeanpaulalexainaude@gmail.com>
2022-05-31 11:48:16 +0200

    message

--- src/main/java/za/co/carols_boutique/ReportBE/IDaoreport/DAORepImp.java
+++ src/main/java/za/co/carols_boutique/ReportBE/IDaoreport/DAORepImp.java
@@ -8,11 +8,15 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 import za.co.carols_boutique.models.Customer;
 import za.co.carols_boutique.models.Report;
 import za.co.carols_boutique.models.Review;
-
+import java.util.Iterator;
+import java.util.Collections;
 /**
  *
  * @author HP
@@ -21,7 +25,7 @@ public class DAORepImp implements DAORep{
     private Connection con;
     private PreparedStatement ps;
     private ResultSet rs;
-    private int RowsAffected; 
+    private int rowsAffected; 
 
     @Override
     public Report viewTopAchievingStores(String month) {
@@ -39,24 +43,31 @@ public class DAORepImp implements DAORep{
         }
         return report;
     }
-
+    //shuffle them and return an x amount of them 
     @Override
     public Report getCustomerReviews(String month, Integer amount) {
+        List<Review> reviews=null;
+        //String id, String comment, Integer rating, Date date
         Report report = null;
         if(con!=null){
             try{
-                ps = con.prepareStatement("");
+                ps = con.prepareStatement("Select id,comment,rating,date from Review");
                 rs=ps.executeQuery();
+                reviews = new ArrayList<>();
                 while(rs.next()){
-                    
+                    reviews.add(new Review(rs.getString("id"),rs.getString(""),rs.getInt(""),rs.getDate("")));
                 }
+                //Iterator it = reviews.iterator();
+                
             }catch(SQLException e){
                 e.printStackTrace();
             }
+            
         }
+//        Collection col = new Collection();
         return report;
     }
-
+    
     @Override
     public Report viewMonthlySales(String storeID, String month) {
         Report report = null;
@@ -93,6 +104,7 @@ public class DAORepImp implements DAORep{
 
     @Override
     public Report viewStoresThatAchievedTarget(String month) {
+        
         Report report = null;
         if(con!=null){
             try{
@@ -178,16 +190,22 @@ public class DAORepImp implements DAORep{
 
     @Override
     public Boolean addReview(Review review) {
-        RowsAffected=0;
+        rowsAffected=0;
         if(con!=null){
             try{
-                ps = con.prepareStatement("");
-                RowsAffected=ps.executeUpdate();
+                //String id, String comment, Integer rating, Date date
+                ps = con.prepareStatement("insert into Review(id,comment,rating,date) values(?,?,?,?)");
+                ps.setString(1, review.getId());
+                ps.setString(2, review.getComment());
+                ps.setInt(3, review.getRating());
+                ps.setDate(4, (java.sql.Date) review.getDate());
+                rowsAffected=ps.executeUpdate();
+                
             }catch(SQLException e){
                 e.printStackTrace();
             }
         }
-        if(RowsAffected==0){
+        if(rowsAffected!=1){
             return false;
         }else{
         return true;
@@ -196,16 +214,21 @@ public class DAORepImp implements DAORep{
 
     @Override
     public Boolean addCustomer(Customer customer) {
-        RowsAffected=0;
+        rowsAffected=0;
+        //String id, String name, String phoneNumber, String email
         if(con!=null){
             try{
-                ps = con.prepareStatement("");
-                RowsAffected=ps.executeUpdate();
+                ps = con.prepareStatement("Insert into Customer(id,name,phoneNumber,email) values(?,?,?,?)");
+                ps.setString(1, customer.getId());
+                ps.setString(2, customer.getName());
+                ps.setString(3, customer.getPhoneNumber());
+                ps.setString(4, customer.getEmail());
+                rowsAffected=ps.executeUpdate();
             }catch(SQLException e){
                 e.printStackTrace();
             }
         }
-        if(RowsAffected==0){
+        if(rowsAffected!=1){
             return false;
         }else{
         return true;
@@ -213,3 +236,4 @@ public class DAORepImp implements DAORep{
     }
     
 }
+
