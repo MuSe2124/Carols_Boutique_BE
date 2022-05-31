32bdb11c74f12fdc87e3cf04419c0f2a7e281153
Mustafaa Osman <Mustafaa Osman@192.168.20.223>
2022-05-31 14:47:51 +0200

    Adding junit tests.

--- src/test/java/za/co/carols_boutique/dao/test/StoreDAOTest.java
+++ src/test/java/za/co/carols_boutique/dao/test/StoreDAOTest.java
@@ -1,6 +1,5 @@
 package za.co.carols_boutique.dao.test;
 
-import org.junit.jupiter.api.Assertions;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -12,26 +11,27 @@ public class StoreDAOTest {
 
 	DAOStore dao;
 	Store store;
+
 	public StoreDAOTest() {
 		this.dao = null;
 		this.store = new Store("TestStoreID", "TestStoreName", "TestStoreLocation", "TestStorePassword");
 	}
-	
+
 	@Test
 	void testNotGetStore() {
 		assertNull(dao.getStore("TestStoreID", "TestStorePassword"));
 	}
-	
+
 	@Test
 	void testAddStore() {
 		assertTrue(dao.addStore(store));
 	}
-	
+
 	@Test
 	void testGetStore() {
 		assertNotNull(dao.getStore("TestStoreID", "TestStorePassword"));
 	}
-	
+
 	@Test
 	void testDeleteStore() {
 		assertTrue(dao.deleteStore("TestStoreID"));
